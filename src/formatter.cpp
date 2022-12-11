930a13f10616f459d41b6b5692df5b3d5513a0e3
artqs01 <kus.artur78@gmail.com>
2022-12-12 00:15:13 +0100

    add main loop in format() function

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -1,13 +1,27 @@
 #include "formatter.hpp"
-#include <cctype>
 #include <cstddef>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <vector>
 #include <algorithm>
 
 namespace stf {
+
+size_t text_chunk::length() const
+{
+	return word.length() + space_length;
+}
+
+std::string text_chunk::format() const
+{
+	std::string formatted_chunk;
+	for (size_t i = 0; i < space_length; i++) {
+		formatted_chunk.push_back(' ');
+	}
+	return formatted_chunk + word;
+}
 
 std::vector<text_chunk> prepare_to_format(const std::string_view& buffer){
 	std::vector<text_chunk> text_chunks;
@@ -32,24 +46,53 @@ std::vector<text_chunk> prepare_to_format(const std::string_view& buffer){
 			text_chunks.push_back({.word = word, .space_length = 1});
 		}
 	} while (buffer_index < buffer.length());
+	text_chunks.front().space_length = 0;
 	return text_chunks;
 }
 
 void print_chunks(const std::vector<text_chunk>& text_chunks)
 {
 	for (const auto& c : text_chunks) {
-		std::cout << c.word << c.word.length() << ", " << c.space_length << "\n";
+		std::cout << c.word << ": " << c.word.length() << ", " << c.space_length << "\n";
 	}
+}
+
+std::string format_line(std::vector<text_chunk>::const_iterator first, std::vector<text_chunk>::const_iterator last)
+{
+	
 }
 
 std::ostream& format(std::istream& in, std::ostream& out, size_t width) {
 	std::stringstream s;
 	s << in.rdbuf();
-	std::vector<text_chunk> text_chunks = prepare_to_format(s.str());
-	print_chunks(text_chunks);
-	return out << "";
+	auto text_chunks = prepare_to_format(s.str());
+	if (text_chunks.empty()) {
+		return out;
+	}
+	auto line_begin = text_chunks.begin();
+	auto line_end = text_chunks.begin();
+	size_t line_length = 0;
+	do {
+		if (line_end == text_chunks.end()) {
+			line_end--;
+			out << format_line(line_begin, line_end);
+			break;
+		}
+		else if (line_length < width) {
+			line_length += line_end->length();
+			line_end++;
+			continue;
+		}
+		else {
+			if (line_begin != line_end) {
+				line_end--;
+			}
+			out << format_line(line_begin, line_end);
+			line_begin = line_end + 1;
+		}
+	} while (line_begin < text_chunks.end());
+	return out;
 }
-
 
 }
 
--- src/formatter.hpp
+++ src/formatter.hpp
@@ -10,23 +10,22 @@
 namespace stf
 {
 
-// class formatter
-// {
-// 	public:
-// 		formatter(std::string_view directory);
-// 	private:
-// 		std::filesystem::path directory = "";
-// 		std::string file_buf = "";
-// };
-
+// Represents single word from a text and space length before actual word
 struct text_chunk
 {
 	std::string word;
+
+	// lenghth od a space before actual word
 	size_t space_length;
+
+	size_t length() const;
+	std::string format() const;
 };
 
 std::vector<text_chunk> prepare_to_format(const std::string_view& buffer);
+std::string format_line(std::vector<text_chunk>::const_iterator first, std::vector<text_chunk>::const_iterator last);
 std::ostream& format(std::istream& in, std::ostream& out, size_t width);
+void print_chunks(const std::vector<text_chunk>& text_chunks);
 
 }
 
